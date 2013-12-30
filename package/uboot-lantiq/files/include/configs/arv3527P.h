8727cad855a22660e559a7a0ae93b1688811cbfc
Álvaro Fernández Rojas <noltari@gmail.com>
2013-12-30 03:47:59 +0100

    [uboot-lantiq] Cleanup configs.

--- package/uboot-lantiq/files/include/configs/arv3527P.h
+++ package/uboot-lantiq/files/include/configs/arv3527P.h
@@ -6,12 +6,8 @@
 
 #define CONFIG_SYS_MAX_RAM	32*1024*1024
 #define CONFIG_USE_DDR_PSC_32   1
-#define	CONFIG_SYS_PROMPT	"ARV3527P => "
+#define	CONFIG_SYS_PROMPT	"ARV3527P # "
 
-/*#define CONFIG_BUTTON_PORT1
-#define CONFIG_BUTTON_PIN	13
-#define CONFIG_BUTTON_LEVEL	0
-*/
 #include "arcadyan-common.h"
 
 #endif
